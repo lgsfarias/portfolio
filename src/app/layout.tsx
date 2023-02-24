eeaf2b9d45bceeabc0d508984f1a1c67ad8618c5
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2023-02-24 11:03:59 -0300

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,16 @@
-import './globals.css'
+import './globals.css';
+import { Roboto_Condensed } from '@next/font/google';
+import Link from 'next/link';
+
+const robotoCondensed = Roboto_Condensed({
+  subsets: ['latin'],
+  weight: ['300', '400', '700'],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +19,22 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className={`${robotoCondensed.className}`}>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
